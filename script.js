7ad789ac98f19cc229b9f342ce46aa863c1d4a33
Artur Gręziak <alex4448@wp.pl>
2021-07-07 23:28:13 +0200

    slider introductory part

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ function myFunction() {
   var element = document.querySelector(".fot_1A");
   element.classList.toggle("mystyle");
 }
+
+function myFunction2() {
+  var element = document.querySelector(".fot_2A");
+  element.classList.toggle("mystyle");
+}
+
+function myFunction3() {
+  var element = document.querySelector(".fot_3A");
+  element.classList.toggle("mystyle");
+}
+
+function myFunction4() {
+  var element = document.querySelector(".fot_4A");
+  element.classList.toggle("mystyle");
+}
+
+function myFunction5() {
+  var element = document.querySelector(".fot_5A");
+  element.classList.toggle("mystyle");
+}
+
+function myFunction6() {
+  var element = document.querySelector(".fot_6A");
+  element.classList.toggle("mystyle");
+}
